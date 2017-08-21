425890a0b030afcbe6a811461b8fcec2153a9767
Thomas Bille <thomas.bille625@gmail.com>
2017-08-21 17:11:54 +0200

    Add bootstrap for vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="navbar">
+    <b-navbar toggleable variant="info" toggle-breakpoint="md">
+
+      <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+
+      <b-navbar-brand href="#">La Braxéenne</b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <b-nav is-nav-bar>
+          <b-nav-item href="#/">Hello</b-nav-item>
+          <b-nav-item href="#/test">Test</b-nav-item>
+        </b-nav>
+
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
