0736d11949defaf87942d3a1e43b63604391bd08
Thomas Bille <thomas.bille625@gmail.com>
2017-08-24 15:51:29 +0200

    Add footer component with right position

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,55 @@
 <template>
   <div id="app">
     <navbar></navbar>
-    <router-view></router-view>
+    <div class="container">
+      <router-view></router-view>
+    </div>
+
+    <footer class="footer">
+      <footer-braxeenne></footer-braxeenne>
+    </footer>
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar'
+import FooterBraxeenne from './components/FooterBraxeenne'
+
 export default {
   name: 'app',
   components: {
-    Navbar
+    Navbar,
+    FooterBraxeenne
   }
 }
 </script>
 
 <style>
+/* Sticky footer styles
+-------------------------------------------------- */
+html {
+  position: relative;
+  min-height: 100%;
+}
+
+body {
+  /* Margin bottom by footer height */
+  margin-bottom: 60px;
+}
+
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  /* Set the fixed height of the footer here */
+  height: 60px;
+  line-height: 60px; /* Vertically center the text there */
+  background-color: #f5f5f5;
+}
+
+.container {
+  width: auto;
+  max-width: 680px;
+  padding: 0 15px;
+}
 </style>
